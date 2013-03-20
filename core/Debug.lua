62fd0f838ca71f93431195669b5d6793714dca99
Ben Thompson <bthomp@gmail.com>
2013-03-20 09:49:51 -0700

    still not working :/

--- core/Debug.lua
+++ core/Debug.lua
@@ -1,14 +1,20 @@
-local __isUnwindingStack = false
 function __instrument_class(t, tname)
+	local __isUnwindingStack = false
+
 	print (("Instrumenting Class %s"):format(tname))
 	local function __instrument_function(f, fname, ...)
+		__isUnwindingStack = false
+
 		local r
 		local p = {...}
-		__isUnwindingStack = false
 		local s, e = pcall(function() r = {f(unpack(p))} end)
 		if not s then
 			if not __isUnwindingStack then
-				print(("Unhandle error caught: %s"):format(tostring(e))
+				if e then
+					print(("Error: %s"):format(tostring(e))
+				else
+					print("Error: <unknown error>")
+				end
 				print(("--> %s"):format(fname))
 				__isUnwindingStack = true
 			else
@@ -16,12 +22,14 @@ function __instrument_class(t, tname)
 			end
 			error(e)
 		end
+		
 		return unpack(r)
 	end
+
 	for fname, f in pairs(t) do
 		if type(f) == "function" then
 			local name = ("%s:%s"):format(tname, fname)
-			t[fname] =  function(...) return __instrument_function(f, name, ...) end
+			t[fname] = function(...) return __instrument_function(f, name, ...) end
 		end
 	end
 end
